252748cca628292370e623bc45d041b294b0567f
Seoyoung <seoyoungiyo@gmail.com>
2023-11-05 02:15:36 +0900

    refactor: test 코드 삭제

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,7 @@
-import { NavBar, UserCard } from 'components';
-import profile from '../assets/profile-image.svg';
+import { NavBar } from 'components';
 
 const HomePage = () => {
-  return (
-    <>
-      <NavBar>질문하러 가기</NavBar>
-      <UserCard src={profile} name="아초는고양이" count="9" />
-    </>
-  );
+  return <NavBar>질문하러 가기</NavBar>;
 };
 
 export default HomePage;
