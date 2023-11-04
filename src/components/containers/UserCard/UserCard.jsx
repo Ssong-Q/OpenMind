68fb3810b60ecccf05bc81221d81526474d86532
seoyoung-min <144652458+seoyoung-min@users.noreply.github.com>
2023-11-05 02:17:38 +0900

    feat: UserCard 모바일 사이즈 추가 (#6)
    * refactor: PageButton 일반 컴포넌트에서 스타일드 컴포넌트로 변경
    
    * feat: UserCard 컴포넌트 PC 버전 추가
    
    * refactor: 기본 meta tag 추가
    
    * feat: UserCard 일부 수정 및 모바일 사이즈 추가
    
    * refactor: 불필요 속성 제거
    
    * refactor: test 코드 삭제

---
+++ src/components/containers/UserCard/UserCard.jsx
@@ -0,0 +1,21 @@
+import * as Styled from './StyleUserCard';
+
+function UserCard({ src, name, count }) {
+  return (
+    <Styled.Container>
+      <Styled.ProfileContainer>
+        <Styled.Img src={src} alt="프로필 이미지" />
+        <Styled.Name>{name}</Styled.Name>
+      </Styled.ProfileContainer>
+      <Styled.InfoContainer>
+        <Styled.Div>
+          <Styled.MessageIcon />
+          <Styled.P>받은 질문</Styled.P>
+        </Styled.Div>
+        <Styled.P>{count}개</Styled.P>
+      </Styled.InfoContainer>
+    </Styled.Container>
+  );
+}
+
+export default UserCard;
