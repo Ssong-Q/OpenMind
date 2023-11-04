9327c35d9c090aec81f648ae73b0adbe0b394994
hyejungan <138510303+hyejungan@users.noreply.github.com>
2023-11-04 20:57:42 +0900

    feat: ButtonBadge 기능 추가 (#3)

---
+++ src/components/common/ButtonBadge/CompleteBadge.jsx
@@ -0,0 +1,7 @@
+import * as Styled from './StyleButtonBadge';
+
+const CompleteBadge = () => {
+  return <Styled.Button color="brown">답변 완료</Styled.Button>;
+};
+
+export default CompleteBadge;
---
+++ src/components/common/ButtonBadge/IncompleteBadge.jsx
@@ -0,0 +1,7 @@
+import * as Styled from './StyleButtonBadge';
+
+const IncompleteBadge = () => {
+  return <Styled.Button>미답변</Styled.Button>;
+};
+
+export default IncompleteBadge;
