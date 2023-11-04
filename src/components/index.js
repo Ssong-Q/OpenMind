c639f969bb4e7751adb8c26c42eeed320d41f71b
Seoyoung <seoyoungiyo@gmail.com>
2023-11-04 23:44:38 +0900

    feat: PageButton 컴포넌트 추가

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as InputFiled } from './common/InputField/InputField';
 export { default as ButtonBox } from './common/ButtonBox/ButtonBox';
 export { default as NavBar } from './containers/NavBar/NavBar';
 export { default as ButtonShare } from './containers/ButtonShare/ButtonShare';
+export { default as PageButton } from './common/PageButton/PageButton';
