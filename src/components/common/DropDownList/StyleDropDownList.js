35172153dd80f5010192308416b3f3588acc3175
seoyoung-min <144652458+seoyoung-min@users.noreply.github.com>
2023-11-05 16:29:19 +0900

    feat: DropDown 컴포넌트 완성 (#10)
    * feat: DropDown div 컴포넌트 추가
    
    * fix: 버튼 클릭시 파란색 배경 하이라이트 제거
    
    * feat: DropDown용 PopOverSort 팝오버 컴포넌트 추가
    
    * feat: DropDown 컴포넌트들 연결하여 하나로 구현
    
    * refactor: DropDown 컴포넌트 폴더 변경 및 관련 파일 명 변경
    
    * design: 버튼 정렬 및 hover 배경색 변경 효과 추가

---
+++ src/components/common/DropDownList/StyleDropDownList.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const Ul = styled.ul`
+  width: 79px;
+  margin-top: 4px;
+  padding: 4px 0px;
+  border-radius: 8px;
+  border: 1px solid var(--gray30);
+  background-color: var(--gray10);
+  box-shadow: var(--shadow-1pt);
+`;
+
+export const Button = styled.button`
+  align-self: stretch;
+  width: 100%;
+  padding: 6px 16px;
+  background-color: transparent;
+  font-size: 1.4rem;
+  font-weight: 500;
+  line-height: 18px;
+  color: ${({ select }) => (select ? `var(--blue)` : `var(--gray50)`)};
+  cursor: pointer;
+
+  &:hover {
+    background-color: var(--gray20);
+  }
+`;
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 export { default as App } from './App';
 export { default as InputFiled } from './common/InputField/InputField';
 export { default as ButtonBox } from './common/ButtonBox/ButtonBox';
-export { default as DropDown } from './common/DropDown/DropDown';
+export { default as DropDown } from './containers/DropDown/DropDown';
 export { default as NavBar } from './containers/NavBar/NavBar';
 export { default as ButtonShare } from './containers/ButtonShare/ButtonShare';
 export { default as UserCard } from './containers/UserCard/UserCard';
