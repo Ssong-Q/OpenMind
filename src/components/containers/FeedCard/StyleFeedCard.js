0ec936cecbb8ef7cb084800c420bbb4a527fa60d
Ahn jisoo <anjisu2001@naver.com>
2023-11-06 23:52:33 +0900

    feat: 답변 있는 경우 카드 컴포넌트 제작

---
+++ src/components/containers/FeedCard/StyleFeedCard.js
@@ -0,0 +1,111 @@
+import styled from 'styled-components';
+
+export const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+  width: 684px;
+  height: 457px;
+  padding: 32px;
+  border-radius: 16px;
+  box-shadow: var(--shadow-1pt);
+  margin: 100px; //삭제
+`;
+
+export const KebabImg = styled.img``;
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Question = styled.div``;
+
+export const QuestionTime = styled.div`
+  color: var(--gray40);
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 18px;
+`;
+
+export const QuestionContent = styled.div`
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 24px;
+`;
+
+export const AnswerContainer = styled.div`
+  display: flex;
+  gap: 12px;
+  width: 100%;
+`;
+
+export const ProfileImg = styled.img`
+  width: 48px;
+  height: 48px;
+`;
+
+export const AnswerBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+`;
+
+export const AnswerProfile = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+export const AnswerName = styled.div`
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 24px;
+`;
+
+export const AnswerDate = styled.div`
+  color: var(--gray40);
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 18px;
+`;
+
+export const AnswerContent = styled.div`
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 22px;
+`;
+
+export const Footer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding-top: 18px;
+  border-top: 1px solid var(--gray30);
+`;
+
+export const FooterIcons = styled.div`
+  display: flex;
+  gap: 32px;
+`;
+
+export const FooterIconContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 6px;
+  var(--gray40)
+`;
+export const FooterIcon = styled.img`
+  width: 16px;
+  height: 16px;
+  color: var(--gray40);
+`;
+
+export const FooterIconContent = styled.div`
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 18px;
+`;
