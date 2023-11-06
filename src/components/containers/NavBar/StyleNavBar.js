ec0cdbb484a40c9d53599c107e7867de2a9df282
seoyoung-min <144652458+seoyoung-min@users.noreply.github.com>
2023-11-06 09:37:29 +0900

    refactor: 홈페이지 레이아웃 및 스타일 수정 (#15)
    * feat: tablet 사이즈 패딩 크기 조절
    
    * refactor: 홈페이지 레이아웃 및 스타일 리팩토링
    
    * refactor: StyleNavBar position fixed에서 absolute로 변경

--- src/components/containers/NavBar/StyleNavBar.js
+++ src/components/containers/NavBar/StyleNavBar.js
@@ -1,12 +1,18 @@
 import styled from 'styled-components';
 
 export const NavBarContainer = styled.div`
-  width: 100%;
-  height: 100px;
-  padding: 25px 125px 9px;
   display: flex;
   justify-content: space-between;
   align-items: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  padding: 45px 125px;
+
+  @media (max-width: 1199px) {
+    padding: 44px 45px;
+  }
 `;
 
 export const NavBarLogo = styled.img`
