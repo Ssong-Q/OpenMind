3e0e409ab724cbeff7f4294b3212d29d572524b6
James Song <skdmlrh12@gmail.com>
2023-11-06 17:34:09 +0900

    refactor: NavBar z-index 추가, StyleQuestionListPage의 cardSectionContainer margin 추가

--- src/components/index.js
+++ src/components/index.js
@@ -16,4 +16,3 @@ export { default as UserCard } from './containers/UserCard/UserCard';
 export { default as ModalListPage } from './containers/Modal/ModalListPage';
 export { default as QuestionList } from './containers/QuestionList/QuestionList';
 export { default as PostHeader } from './containers/PostHeader/PostHeader';
-export { default as ModalListPage } from './containers/Modal/ModalListPage';
