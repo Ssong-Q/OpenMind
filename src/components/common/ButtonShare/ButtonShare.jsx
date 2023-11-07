06c3bcc0d76c2832ff538887fb0de673f4054ee1
Seoyoung <seoyoungiyo@gmail.com>
2023-11-07 20:40:02 +0900

    feat: 페이스북 및 링크 복사 기능 구현 그리고 파일 구조 변경

--- src/components/common/ButtonShare/ButtonShare.jsx
+++ src/components/common/ButtonShare/ButtonShare.jsx
@@ -1,28 +1,30 @@
 import * as Styled from './StyleButtonShare.js';
-import handleShareKakaoClick from 'utils/handleShareKakaoClick.js';
+import * as handle from 'utils/shareSNS.js';
 import linkIcon from 'assets/link-icon.svg';
 import facebookIcon from 'assets/facebook-icon.svg';
 import kakaoIcon from 'assets/kakao-icon.svg';
 
-function ButtonShare({ name, image }) {
+function ButtonShare({ name, src }) {
+  const sharedUrl = window.location.href;
+
   return (
     <Styled.Ul>
       <Styled.Img
         src={linkIcon}
         onClick={() => {
-          handleShareKakaoClick(name, image);
+          handle.copyUrl(sharedUrl);
         }}
       />
       <Styled.Img
         src={kakaoIcon}
         onClick={() => {
-          handleShareKakaoClick(name, image);
+          handle.shareKakao(name, src, sharedUrl);
         }}
       />
       <Styled.Img
         src={facebookIcon}
         onClick={() => {
-          handleShareKakaoClick(name, image);
+          handle.shareFacebook(sharedUrl);
         }}
       />
     </Styled.Ul>
