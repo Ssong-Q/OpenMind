b5e5d9e60fe4a2b5242d138c23cb751789bb367d
Seoyoung <seoyoungiyo@gmail.com>
2023-11-08 18:15:24 +0900

    feat: 링크 복사 후 Toast 5초 보이다 사라지는 기능 구현

--- src/components/common/ButtonShare/ButtonShare.jsx
+++ src/components/common/ButtonShare/ButtonShare.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import Toast from '../Toast/Toast';
 import * as Styled from './StyleButtonShare.js';
 import * as handle from 'utils/shareSNS.js';
 import linkIcon from 'assets/link-icon.svg';
@@ -5,29 +7,47 @@ import facebookIcon from 'assets/facebook-icon.svg';
 import kakaoIcon from 'assets/kakao-icon.svg';
 
 function ButtonShare({ name, src }) {
+  const [isToastOn, setIsToastOn] = useState(false);
+
   const sharedUrl = window.location.href;
 
+  const handleCopyUrl = (url) => {
+    try {
+      handle.copyUrl(url);
+      setIsToastOn(true);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <Styled.Ul>
-      <Styled.Img
-        src={linkIcon}
-        onClick={() => {
-          handle.copyUrl(sharedUrl);
-        }}
-      />
-      <Styled.Img
-        src={kakaoIcon}
-        onClick={() => {
-          handle.shareKakao(name, src, sharedUrl);
-        }}
-      />
-      <Styled.Img
-        src={facebookIcon}
-        onClick={() => {
-          handle.shareFacebook(sharedUrl);
-        }}
-      />
-    </Styled.Ul>
+    <>
+      <Styled.Container>
+        <Styled.Ul>
+          <Styled.Img
+            src={linkIcon}
+            onClick={() => {
+              handleCopyUrl(sharedUrl);
+            }}
+          />
+          <Styled.Img
+            src={kakaoIcon}
+            onClick={() => {
+              handle.shareKakao(name, src, sharedUrl);
+            }}
+          />
+          <Styled.Img
+            src={facebookIcon}
+            onClick={() => {
+              handle.shareFacebook(sharedUrl);
+            }}
+          />
+        </Styled.Ul>
+      </Styled.Container>
+      {isToastOn && (
+        <Toast setStatus={setIsToastOn}>URL이 복사되었습니다</Toast>
+      )}
+    </>
   );
 }
 
