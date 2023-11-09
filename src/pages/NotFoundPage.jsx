48b36f71003bbad762b905bd0305a16b2cd7bf78
Seoyoung <seoyoungiyo@gmail.com>
2023-11-09 17:10:58 +0900

    init: NotFoundPage 초기 커밋

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,5 +1,20 @@
+import { Link } from 'react-router-dom';
+import { LogoImg } from './StyleHomePage';
+import * as Styled from './StyleNotFoundPage';
+import oops from 'https://media.giphy.com/media/1Y7ChRtbWnYONjDidg/giphy.gif?cid=ecf05e478cxycm5lpmezvl1bcx4hz3qcw73d9gwp6blve7zh&ep=v1_gifs_search&rid=giphy.gif&ct=g';
+
 const NotFoundPage = () => {
-  return <div></div>;
+  return (
+    <Styled.Div>
+      <Link to="/">
+        <LogoImg />
+      </Link>
+      <Styled.Container>
+        <h1>Oops! something went wrong</h1>
+        <img src={oops} width={'100'} height={'100'} />
+      </Styled.Container>
+    </Styled.Div>
+  );
 };
 
 export default NotFoundPage;
---
+++ src/pages/StyleNotFoundPage.js
@@ -0,0 +1,13 @@
+import { styled } from 'styled-components';
+
+export const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 60px;
+`;
+
+export const Container = styled.div`
+  padding: 30px;
+  font-size: 10rem;
+`;
