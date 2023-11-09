df3406386bd67f850c8d53389a91f471e2223f63
seoyoung-min <144652458+seoyoung-min@users.noreply.github.com>
2023-11-09 19:58:33 +0900

    feat: Not Found Page 구현 (#34)
    * init: NotFoundPage 초기 커밋
    
    * feat: NotFoundPage 완성
    
    * refactor: gif에 alt 텍스트 삽입

---
+++ src/pages/StyleNotFoundPage.js
@@ -0,0 +1,50 @@
+import { styled } from 'styled-components';
+
+export const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  min-height: 1000px;
+`;
+
+export const Nav = styled.div`
+  text-align: center;
+  margin-top: 60px;
+  width: 100%;
+  height: auto;
+`;
+
+export const ContentContainer = styled.div`
+  flex: 1;
+  flex-shrink: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  gap: 20px;
+  margin-top: -100px;
+`;
+
+export const Logo = styled.img`
+  width: 100%;
+  max-width: 300px;
+  min-width: 200px;
+`;
+
+export const H1 = styled.h1`
+  font-family: Pretendard;
+  font-size: 6rem;
+  font-weight: 500;
+`;
+
+export const H2 = styled(H1)`
+  font-size: 4rem;
+`;
+
+export const P = styled.p`
+  margin-top: 20px;
+  font-size: 1.5rem;
+  color: var(--gray40);
+`;
