057fa6de22cc1d57669c65eba343059f47b3fda6
James Song <skdmlrh12@gmail.com>
2023-11-12 21:47:27 +0900

    refactor: pagination 중복 수정

--- src/components/common/PageButton/Pagination.jsx
+++ src/components/common/PageButton/Pagination.jsx
@@ -3,7 +3,6 @@ import { Link, useLocation } from 'react-router-dom';
 import * as Styled from './StylePageButton';
 import createPageArray from './createPageArray';
 
-
 const Pagination = ({ total, onClick, limit, width }) => {
   const location = useLocation();
   const pageNum = location.pathname.split('/')[2];
@@ -12,9 +11,7 @@ const Pagination = ({ total, onClick, limit, width }) => {
 
   let arrLen = width > 767 ? 7 : 5;
   let pageArr = createPageArray(TOTAL_PAGE, pageNum, arrLen);
-
-  let arrLen = width > 767 ? 7 : 5;
-  let midArr = getPageArray(arrLen, pageArr, pageNum);
+  // let midArr = getPageArray(arrLen, pageArr, pageNum);
 
   const handleButtonClick = (num) => {
     onClick(limit * (num - 1));
