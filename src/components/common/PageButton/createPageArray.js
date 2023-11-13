180534f10379e2973e1ce170766c686afe45dbc3
seoyoung-min <144652458+seoyoung-min@users.noreply.github.com>
2023-11-13 19:20:46 +0900

    feat: pagination 화살표 수정 및 계정삭제시 localStorage 삭제 기능 구현 (#54)

--- src/components/common/PageButton/createPageArray.js
+++ src/components/common/PageButton/createPageArray.js
@@ -1,7 +1,7 @@
 export default function createPageArray(total, pageNum, arrLen) {
   let pageArray = [];
 
-  //1. arrLen<=TotalPage
+  //1. arrLen>=TotalPage
   if (arrLen >= total) {
     for (let i = 1; i <= total; i++) {
       pageArray.push(i);
@@ -9,14 +9,14 @@ export default function createPageArray(total, pageNum, arrLen) {
     return pageArray;
   }
 
-  //2. arrLen>TotalPage
+  //2. arrLen<TotalPage
   let SIDE = Math.floor(arrLen / 2);
   let firstNum = +pageNum - SIDE;
   let lastNum = +pageNum + SIDE;
 
   //배열의 시작 숫자 조정하기
-  if (firstNum <= 2) firstNum = 1;
-  if (lastNum >= total - 1) firstNum = total - (arrLen - 1);
+  if (firstNum < 1) firstNum = 1;
+  if (lastNum > total) firstNum = total - (arrLen - 1);
 
   //배열만들기
   for (let i = firstNum; i <= firstNum + (arrLen - 1); i++) {
