09f6b7bdd5c905189c6b3cd5fb96279165e206ed
KyuKyoung Song (James Song) <122101706+Ssong-Q@users.noreply.github.com>
2023-11-15 15:49:17 +0900

    fix: console.log(theme) 제거 (#60)

--- src/components/containers/FeedCard/FeedCardSection.jsx
+++ src/components/containers/FeedCard/FeedCardSection.jsx
@@ -5,8 +5,6 @@ import { ReactComponent as SantaImg } from 'assets/santa.svg';
 import * as Styled from './StyleFeedCardSection';
 import { ThemeContext } from 'styled-components';
 
-
-
 const FeedCardSection = ({
   total,
   data,
@@ -14,14 +12,16 @@ const FeedCardSection = ({
   setTotal,
   setQuestionData,
 }) => {
-
   const theme = useContext(ThemeContext);
-  console.log(theme);
 
   return (
     <Styled.CardContainer>
       <Styled.CountContainer>
-        {theme.snow ? <SantaImg /> :<MessageImg width="24" height="25" fill="#542F1A" />}
+        {theme.snow ? (
+          <SantaImg />
+        ) : (
+          <MessageImg width="24" height="25" fill="#542F1A" />
+        )}
         <Styled.CountContent>
           {total ? `${total}개의 질문이 있습니다` : `아직 질문이 없습니다`}
         </Styled.CountContent>
