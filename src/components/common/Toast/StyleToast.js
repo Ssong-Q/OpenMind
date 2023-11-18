a5ec1bd3cc94a7473f57c783275e9db5e1f95e6b
Seoyoung <seoyoungiyo@gmail.com>
2023-11-18 15:31:41 +0900

    feat: Toast 메시지 사라질 떄 fadeout 애니메이션 효과 추가

--- src/components/common/Toast/StyleToast.js
+++ src/components/common/Toast/StyleToast.js
@@ -1,4 +1,14 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+//서서히 사라지는 애니메이션
+const fadeout = keyframes`
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0;
+  }
+`;
 
 //'URL이 복사되었습니다' ToastButton으로 사용합니다
 export const Toast = styled.div`
@@ -18,6 +28,8 @@ export const Toast = styled.div`
   line-height: 18px;
   color: var(--gray10);
   box-shadow: var(--shadow-2pt);
+  animation: ${fadeout} 1s;
+  animation-delay: 4s;
 
   @media (max-width: 767px) {
     bottom: 100px;
